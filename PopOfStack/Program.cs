75c6ebaa4280b59fe06c597dd945b7f2af86c248
Luv sahu <lksahu404@gmail.com>
2022-06-23 16:40:48 +0530

    This code for a Pop of Stack Linklist

---
+++ PopOfStack/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace DataStructure
+
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            //LinkedList linked = new LinkedList();
+            //Node node1 = new Node(70);
+            //Node node2 = new Node(56);
+            //Node node3 = new Node(30);
+            //linked.AddNode(node1);
+            //linked.AddNode(node2);
+            //linked.AddNode(node3);
+            //linked.Display();
+
+            LinkedList Link = new LinkedList();
+            Node node1 = new Node(70);
+            Node node2 = new Node(56);
+            Node node3 = new Node(30);
+            Node node4 = new Node(40);
+            Node node5 = new Node(95);
+
+            //Link.AddNode(node1);
+            //Link.AddNode(node2);
+            //Link.AddNode(node3);
+            //Link.Display();
+
+
+            //Console.WriteLine("\n");
+            //Link.AppendNode(node4);
+            //Console.WriteLine("\n");
+            //Link.Display();
+
+
+            //Link.DeleteNodeAtFirst();
+
+            //Link.Display();
+
+            //Console.WriteLine("\n Insert a node Btw");
+            //Link.InsertNode(node5, node2);
+            //Link.Display();
+
+            //Link.DeleteNodeAtLast();
+            //Console.WriteLine("\n Delete at node at last");
+            //Link.Display();
+
+
+
+
+            //Stack
+            Stack stack = new Stack();
+
+            stack.Push(node1);
+            stack.Push(node2);
+            stack.Push(node3);
+            stack.DisplayStack();
+            stack.Top();
+            stack.DisplayStack();
+
+
+
+
+            //Queue
+            Queue queue = new Queue();
+            queue.Enqueue(node1);
+            queue.Enqueue(node2);
+            queue.Enqueue(node3);
+            queue.DisplayQueue();
+            queue.Deenque();
+            queue.DisplayQueue();
+
+        }
+
+
+    }
+
+
+}
+
+
---
+++ PopOfStack/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Stack
+    {
+        LinkedList linkedlist;
+
+        public Stack()
+        {
+            linkedlist = new LinkedList();
+        }
+        public void Push(Node node)
+        {
+            linkedlist.AddNode(node);
+
+        }
+
+        public void DisplayStack()
+        {
+            Console.WriteLine("Stack: ");
+            linkedlist.Display();
+        }
+
+        public void Top()
+        {
+            linkedlist.DeleteNodeAtFirst();
+        }
+    }
+}
