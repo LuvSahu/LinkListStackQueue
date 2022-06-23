881d2090f0a39d03a8edbd4572df54a29c9a742e
Luv sahu <lksahu404@gmail.com>
2022-06-23 17:02:23 +0530

    This is code for Dequeue in Queue

---
+++ DeQueueInQueue/Queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Queue
+    {
+        LinkedList linkedlist;
+
+        public Queue()
+        {
+            linkedlist = new LinkedList();
+        }
+
+
+        public void Enqueue(Node node)
+        {
+            linkedlist.AppendNode(node);
+        }
+        public void DisplayQueue()
+        {
+            Console.WriteLine("Queue :");
+            linkedlist.Display();
+        }
+        public void Top()
+        {
+            var val = linkedlist.Head;
+            Console.WriteLine(val.data);
+        }
+
+        public void Deenque()
+        {
+            linkedlist.DeleteNodeAtLast();
+        }
+
+    }
+}
